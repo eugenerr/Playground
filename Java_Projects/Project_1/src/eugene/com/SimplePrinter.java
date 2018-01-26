2343ddaf34c4c0854fd8acceb2fcc7c71dc3e880
eugener@ca.ibm.com <eugener@ca.ibm.com>
2018-01-26 11:10:28 -0700

    Adding print statement.

--- Java_Projects/Project_1/src/eugene/com/SimplePrinter.java
+++ Java_Projects/Project_1/src/eugene/com/SimplePrinter.java
@@ -3,7 +3,8 @@ package eugene.com;
 public class SimplePrinter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Printing random number: " + Math.random());
 
 	}
 
