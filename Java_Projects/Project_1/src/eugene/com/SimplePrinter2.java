e7d936498930082c5bf12e2801c7559baae087cf
eugener@ca.ibm.com <eugener@ca.ibm.com>
2018-01-26 11:39:49 -0700

    Adding dummy line of code.

--- Java_Projects/Project_1/src/eugene/com/SimplePrinter2.java
+++ Java_Projects/Project_1/src/eugene/com/SimplePrinter2.java
@@ -7,6 +7,7 @@ public class SimplePrinter2 {
 	public static void main(String[] args) {
 		
 		System.out.println("Today is: " + Calendar.getInstance().getTime().toString());
+		System.out.println("Dummy line");
 
 	}
 
