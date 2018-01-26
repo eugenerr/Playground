40835cfe228cb554c00efb70ad7930bacb1e0ce2
eugener@ca.ibm.com <eugener@ca.ibm.com>
2018-01-26 10:33:45 -0700

    Adding SimplePrinter class.

---
+++ Java_Projects/Project_1/src/eugene/com/SimplePrinter.java
@@ -0,0 +1,11 @@
+package eugene.com;
+
+public class SimplePrinter {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Here's random number: " + Math.random() + "!");
+
+	}
+
+}
